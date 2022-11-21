7da0f45f350463e28dac343fdb219558e10a7ebb
icubic <wneimeijer@xs4all.nl>
2022-11-21 02:03:57 +0100

    working on menu actions

--- src/load_ui.cr
+++ src/load_ui.cr
@@ -1,8 +1,9 @@
 require "gtk4"
 
 class ShaderToy
+  #include Enumerable(String)
 
-  property app     : Gtk::Application
+  property app : Gtk::Application
 
   def activate
     filename    = "window.ui"
@@ -26,15 +27,35 @@ class ShaderToy
     exit(@app.run)
   end
 
+  def load_file
+  end
+
+
+  def quit_app
+  end
+
   def load_menu(builder)
+
     filename     = "menu.ui"
     menu_builder = Gtk::Builder.new_from_file("#{__DIR__}/#{filename}")
     menu_model   = Gio::MenuModel.cast menu_builder["app_menu"]
 
+    #
     # attach the menu to the menu button
+    #
     button = Gtk::MenuButton.cast builder["gears"]
     button.menu_model = menu_model
-    #gtk_menu_button_set_menu_model (GTK_MENU_BUTTON (priv->gears), menu);
+
+    # app_actions = [] of Gio::ActionEntry # = ["load", ->load_file, "quit", -> quit_app]
+    #
+    # app_action = Gio::
+    # #app_entries << Gio::ActionEntry.new("quit")
+    # #app_entries =
+    # @app.map_add_action_entries(app_actions)
+
+    # set accelerator for menuitem "Quit"
+    values = ["<Ctrl>Q"]
+    @app.set_accels_for_action("app.quit", values)
   end
 end
 
